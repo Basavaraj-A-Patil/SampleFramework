a907c602869c174f3f4b62725262812160a68339
Basavaraj <basavarajpatil8767@gmail.com>
2024-05-20 17:40:16 +0530

    update

--- SampleFramework/src/test/java/pac1/CreateOrgTest.java
+++ SampleFramework/src/test/java/pac1/CreateOrgTest.java
@@ -1,7 +1,5 @@
 package pac1;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class CreateOrgTest {
@@ -13,11 +11,10 @@ public class CreateOrgTest {
 		System.out.println(System.getProperty("username"));
 		System.out.println(System.getProperty("password"));
 
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.get("http://localhost:8888/");
-		Thread.sleep(2000);
-		driver.quit();
+		/*
+		 * WebDriver driver = new ChromeDriver(); driver.manage().window().maximize();
+		 * driver.get("http://localhost:8888/"); Thread.sleep(2000); driver.quit();
+		 */
 
 	}
 
