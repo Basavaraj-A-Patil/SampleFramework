6f410126969f24c3f0ad4e2427253452130500be
Basavaraj <basavarajpatil8767@gmail.com>
2024-05-20 16:06:50 +0530

    update

--- SampleFramework/src/test/java/pac1/CreateOrgTest.java
+++ SampleFramework/src/test/java/pac1/CreateOrgTest.java
@@ -1,15 +1,23 @@
 package pac1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class CreateOrgTest {
 	@Test(groups = "SmokeTest")
-	public void createOrg() {
+	public void createOrg() throws InterruptedException {
 		System.out.println("execute createOrg");
 		System.out.println(System.getProperty("browser"));
 		System.out.println(System.getProperty("url"));
 		System.out.println(System.getProperty("username"));
 		System.out.println(System.getProperty("password"));
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://localhost:8888/");
+		Thread.sleep(2000);
+		driver.quit();
+		
 	}
 
 	@Test
