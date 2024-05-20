b1a8269e0e1277dd119f3f6801dc3a4b953954cf
Basavaraj <basavarajpatil8767@gmail.com>
2024-05-20 17:56:43 +0530

    update

--- SampleFramework/src/test/java/pac1/CreateOrgTest.java
+++ SampleFramework/src/test/java/pac1/CreateOrgTest.java
@@ -14,11 +14,10 @@ public class CreateOrgTest {
 		System.out.println(System.getProperty("username"));
 		System.out.println(System.getProperty("password"));
 
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.get("http://localhost:8888/");
-		Thread.sleep(2000);
-		driver.quit();
+		/*
+		 * WebDriver driver = new ChromeDriver(); driver.manage().window().maximize();
+		 * driver.get("http://localhost:8888/"); Thread.sleep(2000); driver.quit();
+		 */
 
 	}
 
