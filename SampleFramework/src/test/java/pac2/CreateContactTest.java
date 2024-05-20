6271f6754f122767bf84225aa0b8b51b9466483d
Basavaraj <basavarajpatil8767@gmail.com>
2024-05-20 17:59:30 +0530

    update

--- SampleFramework/src/test/java/pac2/CreateContactTest.java
+++ SampleFramework/src/test/java/pac2/CreateContactTest.java
@@ -1,15 +1,18 @@
 package pac2;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class CreateContactTest {
 	@Test(groups = "SmokeTest")
 	public void createContact() {
 		System.out.println("execute createContact");
+		Assert.fail();
 	}
 
 	@Test
 	public void createContactWithOrg() {
 		System.out.println("execute createContactWithOrg");
+		Assert.fail();
 	}
 }
